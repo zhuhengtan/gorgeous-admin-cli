f6bd917a13ae661edfd7daed74dbe0b574509272
朱恒坦 <hunterzhu@123u.com>
2021-05-06 10:08:32 +0800

    创建项目

--- index.ts
+++ index.ts
@@ -1,8 +1,40 @@
 #! node
+/*
+ * @Date: 2021-04-30 15:06:06
+ * @LastEditors: ZHT
+ * @LastEditTime: 2021-05-06 10:07:47
+ */
+
+const chalk = require('chalk')
 const { program } = require('commander')
+const shell = require('shelljs')
+
 program.version('1.0.0', '-v, --version', '当前版本号')
 
-// 可选参数（[]）项目名，默认为 gorgeous-admin
-program.option('-n, --name [items1]', '创建的项目名', 'gorgeous-admin');
+program.command('create <app-name>')
+.description('创建项目')
+.alias('c')
+.usage('使用说明')
+.action(appName=>{
+  console.log("项目名称: ", appName)
+  // 判断文件夹是否存在
+  if(shell.test('-e', appName)) {
+    return console.log(chalk.red('该名称的文件夹已经存在!'))
+  }
+  // 创建文件夹
+  shell.mkdir(appName)
+
+  const processCwd = process.cwd()
+
+  shell.cp('-R', `${processCwd}/gorgeous-admin-cli/template/*`, `./${appName}`)
+  shell.sed('-i', 'gorgeous-admin', `${appName}`, `./${appName}/package.json`)
+  console.log(chalk.green('正在执行 npm i'))
+  shell.exec(`cd ${appName}/ && npm i`)
+  console.log(chalk.green('执行完毕！'))
+  console.log(chalk.green('项目创建成功！Happy hacking!'))
+})
+
+
+
 
 program.parse(process.argv)
---
+++ template/craco.config.js
@@ -0,0 +1,23 @@
+const path = require('path');
+
+const pathResolve = (pathUrl) => path.join(__dirname, pathUrl);
+
+module.exports = {
+  webpack: {
+    alias: {
+      '@': pathResolve('src'),
+      // 此处是一个示例，实际可根据各自需求配置
+    },
+  },
+  babel: {
+    plugins: [
+      ['import', { libraryName: 'antd', style: true }],
+      ['@babel/plugin-proposal-decorators', { legacy: true }],
+    ],
+  },
+  plugins: [
+    {
+      plugin: require('craco-plugin-scoped-css'),
+    },
+  ],
+};
